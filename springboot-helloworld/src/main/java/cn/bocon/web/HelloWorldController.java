5fe8021cb1b5df5c6a2fd3b18937e7b8084e61a2
tianshishouhu <wangjian309@126.com>
2017-08-05 17:22:37 +0800

    test
    test

---
+++ springboot-helloworld/src/main/java/cn/bocon/web/HelloWorldController.java
@@ -0,0 +1,17 @@
+package cn.bocon.web;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Spring Boot HelloWorld 案例
+ *
+ */
+@RestController
+public class HelloWorldController {
+
+    @RequestMapping("/")
+    public String sayHello() {
+        return "Hello,World!";
+    }
+}
