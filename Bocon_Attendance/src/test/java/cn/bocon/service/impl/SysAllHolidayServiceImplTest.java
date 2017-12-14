de41ff1fec271e3da477f54871c9e3ac8b025605
wangjian <wangjian309@126.com>
2017-12-14 20:50:35 +0800

    test

---
+++ Bocon_Attendance/src/test/java/cn/bocon/service/impl/SysAllHolidayServiceImplTest.java
@@ -0,0 +1,25 @@
+package cn.bocon.service.impl;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import cn.bocon.service.IAttendanceService;
+import cn.bocon.service.ISysAllHolidayService;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+public class SysAllHolidayServiceImplTest {
+	@Autowired
+	private ISysAllHolidayService sysAllHolidayService;
+	
+	@Test
+	public void testInsertHoliday() throws Exception {
+		sysAllHolidayService.insertHoliday();
+	}
+
+}
