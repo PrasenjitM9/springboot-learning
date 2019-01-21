b83ea360ce0a8b1289937fb2fd28e932ff8bba7d
wangjian <wangjian309@126.com>
2019-01-21 17:55:01 +0800

    加入2019年假期

--- bocon_attendance_service/src/main/java/cn/bocon/service/impl/SysAllHolidayServiceImpl.java
+++ bocon_attendance_service/src/main/java/cn/bocon/service/impl/SysAllHolidayServiceImpl.java
@@ -26,14 +26,14 @@ import com.google.common.collect.Maps;
  * </p>
  *
  * @author wangjian
- * @since 2018-10-27
+ * @since 2019-10-27
  */
 @Service
 public class SysAllHolidayServiceImpl extends ServiceImpl<SysAllHolidayMapper, SysAllHoliday> implements ISysAllHolidayService {
 	public void insertHoliday() throws Exception {
         DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-        Date start = sdf.parse("2018-01-01");//开始时间
-        Date end = sdf.parse("2018-12-31");//结束时间
+        Date start = sdf.parse("2019-01-01");//开始时间
+        Date end = sdf.parse("2019-12-31");//结束时间
         List<Date> lists = DateUtil.dateSplit(start, end);
         
         //-------------------插入周末时间---------------
@@ -55,42 +55,40 @@ public class SysAllHolidayServiceImpl extends ServiceImpl<SysAllHolidayMapper, S
         //---------------插入节假日时间------------------
         List<SysAllHoliday> holidays = new ArrayList<SysAllHoliday>();
         
-        holidays.add(new SysAllHoliday("元旦节", "2018-01-01"));
+        holidays.add(new SysAllHoliday("元旦节", "2019-01-01"));
         
-        holidays.add(new SysAllHoliday("春节", "2018-02-13")); 
-        holidays.add(new SysAllHoliday("春节", "2018-02-14")); 
-        holidays.add(new SysAllHoliday("春节", "2018-02-15")); 
-        holidays.add(new SysAllHoliday("春节", "2018-02-16")); 
-        holidays.add(new SysAllHoliday("春节", "2018-02-17")); 
-        holidays.add(new SysAllHoliday("春节", "2018-02-18")); 
-        holidays.add(new SysAllHoliday("春节", "2018-02-19")); 
-        holidays.add(new SysAllHoliday("春节", "2018-02-20")); 
-        holidays.add(new SysAllHoliday("春节", "2018-02-21")); 
-        holidays.add(new SysAllHoliday("春节", "2018-02-22")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-01")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-02")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-03")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-04")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-05")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-06")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-07")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-08")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-09")); 
+        holidays.add(new SysAllHoliday("春节", "2019-02-10")); 
         
-        holidays.add(new SysAllHoliday("清明节", "2018-04-05"));   
-        holidays.add(new SysAllHoliday("清明节", "2018-04-06"));   
-        holidays.add(new SysAllHoliday("清明节", "2018-04-07"));   
+        holidays.add(new SysAllHoliday("清明节", "2019-04-05"));   
+        holidays.add(new SysAllHoliday("清明节", "2019-04-06"));   
+        holidays.add(new SysAllHoliday("清明节", "2019-04-07"));   
         
-        holidays.add(new SysAllHoliday("劳动节", "2018-04-29"));
-        holidays.add(new SysAllHoliday("劳动节", "2018-04-30"));
-        holidays.add(new SysAllHoliday("劳动节", "2018-05-01"));
+        holidays.add(new SysAllHoliday("劳动节", "2019-05-01"));
         
-        holidays.add(new SysAllHoliday("端午节", "2018-06-16"));
-        holidays.add(new SysAllHoliday("端午节", "2018-06-17"));
-        holidays.add(new SysAllHoliday("端午节", "2018-06-18"));
+        holidays.add(new SysAllHoliday("端午节", "2019-06-07"));
+        holidays.add(new SysAllHoliday("端午节", "2019-06-08"));
+        holidays.add(new SysAllHoliday("端午节", "2019-06-09"));
         
-        holidays.add(new SysAllHoliday("中秋节", "2018-09-22"));    
-        holidays.add(new SysAllHoliday("中秋节", "2018-09-23"));   
-        holidays.add(new SysAllHoliday("中秋节", "2018-09-24"));   
+        holidays.add(new SysAllHoliday("中秋节", "2019-09-13"));    
+        holidays.add(new SysAllHoliday("中秋节", "2019-09-14"));   
+        holidays.add(new SysAllHoliday("中秋节", "2019-09-15"));   
         
-        holidays.add(new SysAllHoliday("国庆节", "2018-10-01"));
-        holidays.add(new SysAllHoliday("国庆节", "2018-10-02"));
-        holidays.add(new SysAllHoliday("国庆节", "2018-10-03"));
-        holidays.add(new SysAllHoliday("国庆节", "2018-10-04"));
-        holidays.add(new SysAllHoliday("国庆节", "2018-10-05"));
-        holidays.add(new SysAllHoliday("国庆节", "2018-10-06"));
-        holidays.add(new SysAllHoliday("国庆节", "2018-10-07"));
+        holidays.add(new SysAllHoliday("国庆节", "2019-10-01"));
+        holidays.add(new SysAllHoliday("国庆节", "2019-10-02"));
+        holidays.add(new SysAllHoliday("国庆节", "2019-10-03"));
+        holidays.add(new SysAllHoliday("国庆节", "2019-10-04"));
+        holidays.add(new SysAllHoliday("国庆节", "2019-10-05"));
+        holidays.add(new SysAllHoliday("国庆节", "2019-10-06"));
+        holidays.add(new SysAllHoliday("国庆节", "2019-10-07"));
         
         for(SysAllHoliday day:holidays) {
      	   //跟周末冲突的，不重复插入
@@ -105,15 +103,9 @@ public class SysAllHolidayServiceImpl extends ServiceImpl<SysAllHolidayMapper, S
         
         //-------------- 剔除补班时间(周末需要补班的)---------------------
         List<SysAllHoliday> workDays = new ArrayList<SysAllHoliday>();
-        workDays.add(new SysAllHoliday("补班", "2018-02-11")); //补春节
-        workDays.add(new SysAllHoliday("补班", "2018-02-24")); //补春节
         
-        workDays.add(new SysAllHoliday("补班", "2018-04-08")); //补清明节
-        
-        workDays.add(new SysAllHoliday("补班", "2018-04-28"));
-        
-        workDays.add(new SysAllHoliday("补班", "2018-09-29"));
-        workDays.add(new SysAllHoliday("补班", "2018-09-30"));
+        workDays.add(new SysAllHoliday("补班", "2019-09-29")); //补国庆
+        workDays.add(new SysAllHoliday("补班", "2019-10-12")); //补国庆
         
         for(SysAllHoliday day:workDays) {
      	   System.out.println("剔除日期：" + day.getHolidayDate() + "," + day.getTitle());
